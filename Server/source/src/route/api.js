bbc55c7ea1d9cc4ec8ed1726f41fbb8e5401198a
ToTranSonBa <84238706+ToTranSonBa@users.noreply.github.com>
2022-12-22 23:46:11 +0700

    sua apicontroller

--- Server/source/src/route/api.js
+++ Server/source/src/route/api.js
@@ -5,16 +5,17 @@ let router = express.Router();
 
 const initAPIRoute = (app) =>{
     router.get('/food', apiController.getFood);
-    router.post('/login', apiController.handleLogin);
-    router.post('/register-user', apiController.handleRegisterUser);
-    router.post('/add-to-cart', apiController.handleAddToCart);
-    router.post('/cart', apiController.getCart);
+    router.post('/login', apiController.handleLogin); //{UNAME, PWD}
+    router.post('/register-user', apiController.handleRegisterUser); //{UNAME, PWD, MSSV, SDT, EMAIL}
+    router.post('/add-to-cart', apiController.handleAddToCart); // {TP_MA, MSSV}
+    router.post('/cart', apiController.getCart); // MSSV
     router.put('/raise-number-of-food-in-cart', apiController.handleRaiseNumOfFoodInCart);
     router.put('/reduce-number-of-food-in-cart', apiController.handleReduceNumOfFoodInCart);
     router.delete('/remove-from-cart/:GH_ID', apiController.handleRemoveFromCart);
     router.post('/handle-receipt', apiController.handleReceipt);
     router.post('/get-receipt', apiController.getReceipt);
     router.post('/get-detail-receipt', apiController.getDetailReceipt);
+    // router.post('/employee/report-on-day', apiController.handleReportnDay)
     return app.use('/api/', router);
 }
 
